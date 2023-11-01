900160b48e6298c0f4e26666f44fec0b3f43aa2a
EyitopeIO <adeyitope.io@gmail.com>
2023-11-01 22:53:30 +0000

    fix: Fit characters within screen width.

--- examples_segger/ble_peripheral/ble_app_template/pca10028/s130/ses_official/user_views.cpp
+++ examples_segger/ble_peripheral/ble_app_template/pca10028/s130/ses_official/user_views.cpp
@@ -8,25 +8,25 @@
 
 
 static const char *view_0_acceletation_only =
-  "Acceleration\n\n"
-  "x: %.2f       \n"
-  "y: %.2f       \n"
-  "z: %.2f       \n";
+"Acc.   \n"
+"x: %.2f\n"
+"y: %.2f\n"
+"z: %.2f\n";
 
 static const char *view_1_angle_only =
-  "Angle       \n\n"
-  "x: %.2f       \n"
-  "y: %.2f       \n"
-  "z: %.2f       \n";
+"Angle  \n"
+"x: %.2f\n"
+"y: %.2f\n"
+"z: %.2f\n";
 
 static const char *view_2_angularvelocity_only =
-  "rad/s        \n\n"
-  "x: %.2f        \n"
-  "y: %.2f        \n"
-  "z: %.2f        \n";
+"rad/s  \n"
+"x: %.2f\n"
+"y: %.2f\n"
+"z: %.2f\n";
 
 static const char *view_3_notification =
-  "NOTICE       \n\n %s";
+"NOTICE\n\n %s";
 
 
 /// @brief Array of user views
